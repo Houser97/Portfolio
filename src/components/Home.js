e4dc3d5bf7e2f9dcf873a91ad4146f67bec6b035
Houser97 <arturo.riverar97@gmail.com>
2022-11-02 17:34:25 -0600

    Import Presentation component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/Home.css'
+import { Presentation } from './Presentation'
 
 function Home() {
   return (
     <div className='Home'>
         <div className='home-subcontainer'>
-
+          <Presentation />
         </div>
     </div>
   )
