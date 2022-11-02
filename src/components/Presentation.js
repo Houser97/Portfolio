371773c05ec07a01718d840510cecf4a100a3cc8
Houser97 <arturo.riverar97@gmail.com>
2022-11-02 17:35:06 -0600

    Set component elements

---
+++ src/components/Presentation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/Presentation.css'
+
+export const Presentation = () => {
+  return (
+    <div className='app_presentation'>
+        <div className='app_presentation-div hi-card'>
+            Hi!
+        </div>
+        <div className='app_presentation-div name-card'>
+            I'm Arturo
+        </div>
+        <div className='app_presentation-div lastname-card'>
+            Rivera
+        </div>
+    </div>
+  )
+}
