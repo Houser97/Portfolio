07ddebe8b58fc5d940ec97ca85a44c89bf69d27a
Houser97 <arturo.riverar97@gmail.com>
2022-11-02 11:19:50 -0600

    Define toggle state to hide svgs when clicking their container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Header.css'
 import {HiMenuAlt4} from 'react-icons/hi'
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <header>
         <div className='app__header-subcontainer'>
@@ -12,11 +14,11 @@ const Header = () => {
                 <a className='Projects-btn btn'>Projects</a>
             </div>
 
-            <div className='app_header-btn-toggle'>
-              <svg className="app_header-hamburger app_header-svg" viewBox="0 0 24 24">
+            <div className='app_header-btn-toggle' onClick={() => setToggle(prev => !prev)}>
+              <svg className={`app_header-hamburger app_header-svg ${toggle ? 'hide-svg' : ''}`} viewBox="0 0 24 24">
                   <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
               </svg>
-              <svg className='app_header-close app_header-svg' viewBox="0 0 24 24">
+              <svg className={`app_header-close app_header-svg ${toggle ? 'show-svg' : 'hide-svg'}`} viewBox="0 0 24 24">
                   <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
               </svg>
             </div>
