49d56cd690ddabc59e71fcc6e9de6152abb8ade5
Houser97 <arturo.riverar97@gmail.com>
2022-11-02 19:13:38 -0600

    Add classes for animation with the 'mounted' state

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Presentation.css'
 
 export const Presentation = () => {
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+    
   return (
     <div className='app_presentation'>
-        <div className='app_presentation-div hi-card'>
+        <div className={`app_presentation-div hi-card ${mounted ? 'animation-hi' : ''}`}>
             Hi!
         </div>
-        <div className='app_presentation-div name-card'>
+        <div className={`app_presentation-div name-card ${mounted ? 'animation-name' : ''}`}>
             I'm Arturo
         </div>
-        <div className='app_presentation-div lastname-card'>
+        <div className={`app_presentation-div lastname-card ${mounted ? 'animation-lname' : ''}`}>
             Rivera
         </div>
     </div>
