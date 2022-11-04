5ac6388333ed6b7ec4365ea18047d259076524a9
Houser97 <arturo.riverar97@gmail.com>
2022-11-03 20:35:58 -0600

    Import About component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { About } from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <About />
     </div>
   );
 }
