b196e6730fec3bc863277d134b2c17313f969a10
Houser97 <arturo.riverar97@gmail.com>
2022-11-03 20:34:16 -0600

    Set up component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/About.css'
+
+export const About = () => {
+  return (
+    <div className='app__about'>
+        <div className='app_about-subcontainer'>
+            <h1>About</h1>
+        </div>
+    </div>
+  )
+}
