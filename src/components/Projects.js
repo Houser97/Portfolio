b39e7e60948e18f9a1ba621746f991b6a7f3e648
Houser97 <arturo.riverar97@gmail.com>
2022-11-05 16:59:17 -0600

    Set up component

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Projects.css'
+
+const Projects = () => {
+  return (
+    <div className='app__projects'>
+        <div className='app__projects-subcontainer'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Projects
