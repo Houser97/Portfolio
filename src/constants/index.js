e20d66ac014fe7ad9815be4275488aa17d33c06a
Houser97 <arturo.riverar97@gmail.com>
2022-11-04 22:26:31 -0600

    Create and export array containing objects for each icon

---
+++ src/constants/index.js
@@ -0,0 +1,50 @@
+export const icons = [
+    {
+        class: 'devicon-html5-plain colored icon',
+        text: 'HTML'
+    },
+    {
+        class: 'devicon-css3-plain colored icon',
+        text: 'CSS'
+    },
+    {
+        class: 'devicon-javascript-plain colored icon',
+        text: 'javascript'
+    },
+    {
+        class: 'devicon-git-plain colored icon',
+        text: 'GIT'
+    },
+    {
+        class: 'devicon-react-original colored icon',
+        text: 'React'
+    },
+    {
+        class: 'devicon-nodejs-plain colored icon',
+        text: 'Node.js'
+    },
+    {
+        class: 'devicon-express-original colored icon express',
+        text: 'Express.js'
+    },
+    {
+        class: 'devicon-mongodb-plain colored icon',
+        text: 'MongoDB'
+    },
+    {
+        class: 'devicon-jest-plain colored icon',
+        text: 'Jest'
+    },
+    {
+        class: 'devicon-webpack-plain colored icon',
+        text: 'Webpack'
+    },
+    {
+        class: 'devicon-npm-original-wordmark colored icon',
+        text: 'npm'
+    },
+    {
+        class: 'devicon-heroku-plain colored icon',
+        text: 'Heroku'
+    },
+]
