5865568d8cdbcdc6b00b5d8327bf008511cb7b64
Houser97 <arturo.riverar97@gmail.com>
2022-11-04 21:39:10 -0600

    Wrap text and container for the upcoming cards about technologies

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,16 @@ export const About = () => {
     <div className='app__about' id = 'about'>
         <div className='app_about-subcontainer'>
             <h1 className='about-title'>About</h1>
-            <div className='about-text'>
-                Mechatronics engineering graduate who undertook the challenge
-                of learning web development in a self-taugh way. Having said that,
-                I am passionate about the world of technology and the wonders that
-                ingenuity and creativity can bring.
+            <div className='personal-data-container'>
+              <div className='about-text'>
+                  Mechatronics engineering graduate who undertook the challenge
+                  of learning web development in a self-taugh way. Having said that,
+                  I am passionate about the world of technology and the wonders that
+                  ingenuity and creativity can bring.
+              </div>
+              <div className='technologies-container'>
+
+              </div>
             </div>
         </div>
     </div>
