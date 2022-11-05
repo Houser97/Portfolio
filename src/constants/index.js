bdb63cb4e53f8584d76a86ef20351255cf7fb6b0
Houser97 <arturo.riverar97@gmail.com>
2022-11-04 22:35:35 -0600

    Rename the class field for classIcon

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,50 +1,50 @@
 export const icons = [
     {
-        class: 'devicon-html5-plain colored icon',
+        classIcon: 'devicon-html5-plain colored icon',
         text: 'HTML'
     },
     {
-        class: 'devicon-css3-plain colored icon',
+        classIcon: 'devicon-css3-plain colored icon',
         text: 'CSS'
     },
     {
-        class: 'devicon-javascript-plain colored icon',
+        classIcon: 'devicon-javascript-plain colored icon',
         text: 'javascript'
     },
     {
-        class: 'devicon-git-plain colored icon',
+        classIcon: 'devicon-git-plain colored icon',
         text: 'GIT'
     },
     {
-        class: 'devicon-react-original colored icon',
+        classIcon: 'devicon-react-original colored icon',
         text: 'React'
     },
     {
-        class: 'devicon-nodejs-plain colored icon',
+        classIcon: 'devicon-nodejs-plain colored icon',
         text: 'Node.js'
     },
     {
-        class: 'devicon-express-original colored icon express',
+        classIcon: 'devicon-express-original colored icon express',
         text: 'Express.js'
     },
     {
-        class: 'devicon-mongodb-plain colored icon',
+        classIcon: 'devicon-mongodb-plain colored icon',
         text: 'MongoDB'
     },
     {
-        class: 'devicon-jest-plain colored icon',
+        classIcon: 'devicon-jest-plain colored icon',
         text: 'Jest'
     },
     {
-        class: 'devicon-webpack-plain colored icon',
+        classIcon: 'devicon-webpack-plain colored icon',
         text: 'Webpack'
     },
     {
-        class: 'devicon-npm-original-wordmark colored icon',
+        classIcon: 'devicon-npm-original-wordmark colored icon',
         text: 'npm'
     },
     {
-        class: 'devicon-heroku-plain colored icon',
+        classIcon: 'devicon-heroku-plain colored icon',
         text: 'Heroku'
     },
 ]
