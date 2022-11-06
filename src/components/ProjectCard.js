17af08ad6ef934d04ef31cda7428a7066d45cd39
Houser97 <arturo.riverar97@gmail.com>
2022-11-05 18:35:37 -0600

    Set up component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/ProjectCard.css'
+
+const ProjectCard = () => {
+  return (
+    <div className='app__project-card'>
+        <div className='project-img-container'>
+            <span className='project-card-decoration'></span>
+            <div className='project-img'></div>
+        </div>
+        <div className='project-description'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
