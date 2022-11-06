f788c4d9f267cc9e440be7a68e049b67e04e1359
Houser97 <arturo.riverar97@gmail.com>
2022-11-06 13:02:09 -0600

    Import gsap and ScrollTrigger and set them with useEffect

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import gsap from 'gsap'
+import React, { useEffect, useRef } from 'react'
 import '../styles/Projects.css'
 import ProjectCard from './ProjectCard'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Projects = () => {
+
+  const projects = useRef(null)
+
+  useEffect(() => {
+    const ref = projects.current
+
+    gsap.set(ref, {
+      autoAlpha: 0,
+      y:50,
+    })
+
+    gsap.to(ref, {
+      scrollTrigger: {
+        trigger: ref,
+      },
+      duration: 1,
+      autoAlpha: 1,
+      y:0,
+      ease: 'power3.out',
+    })
+  
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+    }
+  }, [])
+  
+
   return (
     <div className='app__projects'>
-        <div className='app__projects-subcontainer'>
+        <div ref={projects} className='app__projects-subcontainer'>
             <ProjectCard reversed = {false}/>
             <ProjectCard reversed = {true}/>
         </div>
