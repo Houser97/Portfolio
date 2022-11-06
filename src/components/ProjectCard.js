0c4ba72c6fe86d17ad681aa382469addf3dd860c
Houser97 <arturo.riverar97@gmail.com>
2022-11-05 19:11:50 -0600

    Add reversed version for the card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import '../styles/ProjectCard.css'
 
-const ProjectCard = () => {
+const ProjectCard = ({reversed}) => {
   return (
     <div className='app__project-card'>
-        <div className='project-img-container'>
-            <span className='project-card-decoration'></span>
-            <div className='project-img'></div>
-        </div>
-        <div className='project-description'>
-            
-        </div>
+        {!reversed ?  
+            <div className='app__project-subcontainer'>
+                <div className='project-img-container'>
+                    <span className='project-card-decoration'></span>
+                    <div className='project-img'></div>
+                </div>
+                <div className='project-description'>
+
+                </div>
+            </div>
+        : 
+            <div className='app__project-subcontainer-reversed'>
+                <div className='project-description'>
+
+                </div>
+                <div className='project-img-container'>
+                    <span className='project-card-decoration-reversed'></span>
+                    <div className='project-img'></div>
+                </div>
+            </div>
+        }
     </div>
   )
 }
