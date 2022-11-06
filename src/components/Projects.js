c8aa47d9e068fbbb6847a464b7393d1482d71809
Houser97 <arturo.riverar97@gmail.com>
2022-11-05 19:15:06 -0600

    Import ProjectCard component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/Projects.css'
+import ProjectCard from './ProjectCard'
 
 const Projects = () => {
   return (
     <div className='app__projects'>
         <div className='app__projects-subcontainer'>
-
+            <ProjectCard reversed = {false}/>
+            <ProjectCard reversed = {true}/>
         </div>
     </div>
   )
