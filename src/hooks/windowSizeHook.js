3c1ab8a0f5009ac77405bbc9d317aa30041f6136
Houser97 <arturo.riverar97@gmail.com>
2022-11-22 20:21:17 -0600

    Set prop and its function to store current window width and height

---
+++ src/hooks/windowSizeHook.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function useWindowSize(){
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined, 
+    })
+
+    useEffect(() => {
+
+    
+      return () => {
+        second
+      }
+    }, [third])
+    
+}
