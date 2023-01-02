cf3dbeb5285606aba931c8dbe49bd51de6b19df1
Houser97 <arturo.riverar97@gmail.com>
2023-01-01 22:17:29 -0600

    Handle icons when coming from Devicon

--- src/components/TechCard.js
+++ src/components/TechCard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../styles/TechCard.css'
 
-const TechCard = ({classIcon, text}) => {
+const TechCard = ({classIcon, text, isDevicon}) => {
   return (
     <div className='app__TechCard'>
-        <i className={classIcon}></i>
+        {isDevicon ? 
+          (<i className={classIcon}></i>)
+          :
+          (<img src={classIcon} className = 'img-tech'></img>
+        )}
         <div className='app__TechCard-text'>{text}</div>
     </div>
   )
