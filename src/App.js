ccd9634dca5d6da45387a59d7d6951b58da890d6
Houser97 <arturo.riverar97@gmail.com>
2023-01-03 13:00:44 -0600

    Import and set Footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import { About } from './components/About';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <Footer />
     </div>
   );
 }
