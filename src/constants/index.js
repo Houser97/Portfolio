1b368a2e3f382990299cb09ccdb3479848923f75
Houser97 <arturo.riverar97@gmail.com>
2023-01-03 11:50:47 -0600

    Add title field to projects object and add Where is Waldo description

--- src/constants/index.js
+++ src/constants/index.js
@@ -91,6 +91,7 @@ export const icons = [
 
 export const projects = [
     {
+        title: 'My Blog-API',
         image: BlogAPI,
         description: 'This project takes advantage of the functionalities of the REST API concept to communicate the client (React) with the server (Node JS). In addition, it makes use of a JWT token to authenticate the blog owner and grant editing, deletion and post creation capabilities.',
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'JWT'],
@@ -98,6 +99,7 @@ export const projects = [
         preview: 'https://my-blog-api.onrender.com',
     },
     {
+        title: 'Members Only',
         image: MembersOnly,
         description: 'Aside from working with Node as backend and ejs, passport is used to add security to users signing up by means of salting.',
         technologies: ['MongoDB', 'Express.js', 'Node.js', 'Passport'],
@@ -105,6 +107,7 @@ export const projects = [
         preview: 'https://members-only-r266.onrender.com',
     },
     {
+        title: 'Disney Replica',
         image: Disney,
         description: 'Aside from working with Node as backend and ejs, passport is used to add security to users signing up by means of salting.',
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'Passport'],
@@ -112,8 +115,9 @@ export const projects = [
         preview: 'https://disneyplus-54yk.onrender.com/',
     },
     {
+        title: 'Where is Waldo',
         image: Waldo,
-        description: "This application is based on the game Where's Waldo, which consists of finding certain characters in an image full of various elements. The application has a timer to measure the time it takes users to find the three characters. The accumulated time is saved in the database along with a username chosen by the player when all the characters have been found. On the other hand, the game has responsive qualities, since the image can be adapted to different screen sizes without losing its radius of appearance. ",
+        description: "This application is based on the game Where's Waldo, which consists of finding certain characters in an image full of various elements. The application has a timer to measure the time it takes users to find the three characters. The accumulated time is saved in the database along with a username. On the other hand, the game has responsive qualities, since the image can be adapted to different screen sizes without losing its radius of appearance.",
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js'],
         github: 'https://github.com/Houser97/Where-is-walso-V2',
         preview: 'https://whereisgame.onrender.com/',
