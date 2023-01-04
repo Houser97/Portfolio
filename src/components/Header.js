92a61d1787f645618c59c94d330b0727de8cbf51
Houser97 <arturo.riverar97@gmail.com>
2023-01-03 19:08:35 -0600

    Import and set ToggleButton and NavLinks components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/Header.css'
+import NavLinks from './NavLinks'
+import ToggleButton from './ToggleButton'
 
 const Header = () => {
   const [toggle, setToggle] = useState(false)
@@ -26,35 +28,16 @@ const Header = () => {
         <div className={`header-backgound ${isAtTop ? '':'not-at-top-header'}`}></div>
         <div className={`app__header-subcontainer ${isAtTop ? '':'not-at-top'}`}>
             <div className='app_header-logo'>Logo</div>
+
             <div className='app__header-btns'>
-                <a href='#home' className={`Home-btn btn ${activeLink === 'Home' ? 'selected-link' : ''}`}
-                onClick={() => setActiveLink('Home')}>Home</a>
-                <a href='#about' className={`About-btn btn ${activeLink === 'About' ? 'selected-link' : ''}`}
-                onClick={() => setActiveLink('About')}>About</a>
-                <a href='#projects' className={`Projects-btn btn ${activeLink === 'Projects' ? 'selected-link' : ''}`}
-                onClick={() => setActiveLink('Projects')}>Projects</a>
+              <NavLinks setActiveLink={setActiveLink} activeLink = {activeLink}/>
             </div>
 
-            <div className='app_header-btn-toggle' onClick={() => setToggle(prev => !prev)}>
-              {/*<svg className={`app_header-hamburger app_header-svg ${toggle ? 'hide-svg' : ''}`} viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
-              </svg>
-              <svg className={`app_header-close app_header-svg ${toggle ? 'show-svg' : 'hide-svg'}`} viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-              </svg>*/}
-              <span className={`upper-line-nav ${toggle ? 'upper-animate return-animation' : 'line-navbar'}`}></span>
-              <span className={`middle-line-nav ${toggle ? 'middle-animate return-animation' : 'line-navbar'}`}></span>
-              <span className={`lower-line-nav ${toggle ? 'lower-animate return-animation' : 'line-navbar'}`}></span>
-            </div>
+            <ToggleButton toggle = {toggle} setToggle ={setToggle}/>
 
             <nav className={`${toggle ? `${isAtTop ? 'show-nav-at-top':'show-nav-not-at-top'}`:''}`}>
-               <a href='#home' className={`Home-btn btn ${activeLink === 'Home' ? 'selected-link' : ''}`}
-                onClick={() => setActiveLink('Home')}>Home</a>
-                <a href='#about' className={`About-btn btn ${activeLink === 'About' ? 'selected-link' : ''}`}
-                onClick={() => setActiveLink('About')}>About</a>
-                <a href='#projects' className={`Projects-btn btn ${activeLink === 'Projects' ? 'selected-link' : ''}`}
-                onClick={() => setActiveLink('Projects')}>Projects</a>
-                <div className={`helper-nav ${isAtTop ? 'delay-background':'not-at-top-nav-helper'} `}></div>
+              <NavLinks setActiveLink={setActiveLink} activeLink = {activeLink}/>
+              <div className={`helper-nav ${isAtTop ? 'delay-background':'not-at-top-nav-helper'} `}></div>
             </nav>
         </div>
     </header>
