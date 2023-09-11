e0c41f3b8b6749722710972cbdc26cb77e6272f8
Arturo Rivera <arturo.riverar97@gmail.com>
2023-09-10 19:58:36 -0600

    Touch up about text

--- src/components/About.js
+++ src/components/About.js
@@ -12,12 +12,7 @@ export const About = () => {
             <SectionTitle title={'About'}/>
             <div className='personal-data-container'>
               <div className='about-text'>
-                Hello, my name is Arturo Rivera, a mechatronics engineer whose interest has gravitated to 
-                the programming area. Web development, especially, captured my interest for all the 
-                technologies involved and the great opportunity to develop my problem solving skills, 
-                which motivated me to learn it in a self-taught way. My learning is still ongoing and 
-                always will be, but this journey has helped me develop study habits to make the most of my 
-                time and keep pace with new emerging technologies and best practices.
+              Hello, I'm Arturo Rivera, a mechatronics engineer with a strong passion for web development. I'm on a continuous learning journey in this field, driven by the fascination for the technologies it encompasses and the opportunities it provides to enhance problem-solving skills. I'm self-taught, and this path has not only equipped me with the ability to adapt to evolving technologies but has also honed my study habits for efficient learning. I'm committed to staying up-to-date with emerging technologies and best practices as I continue to explore the world of web development.
               </div>
               <div className='technologies-container'>
                 {
