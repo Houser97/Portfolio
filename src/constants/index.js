6c2a1f4283782753010b21c8bc84f022bc4ac49f
Arturo <arturo.riverar97@gmail.com>
2023-09-14 08:43:32 -0600

    Touch up projects description

--- src/constants/index.js
+++ src/constants/index.js
@@ -115,8 +115,7 @@ export const projects = [
     {
         title: `Weather App React And React Native`,
         image: Weather,
-        description: `This project showcases weather-related information sourced from the OpenWeatherMap API, which is based on the user's specified city. The application provides essential weather data, including: Temperature, available in both Celsius and Fahrenheit, Humidity levels, Atmospheric pressure, Wind speed, Sunrise and Sunset time, Feels Like Temperature, Min & Max Temperature,
-        Current UV index. For this project, two versions were developed:
+        description: `This project showcases weather-related information sourced from the OpenWeatherMap API, which is based on the user's specified city. The application provides essential weather data, including: Temperature (available in both Celsius and Fahrenheit), Humidity levels, Atmospheric pressure, Wind speed, Sunrise and Sunset time, Feels Like Temperature, Min & Max Temperature, Current UV index. For this project, two versions were developed:
 
         \u2022    The mobile version using React Native, created with expo
         \u2022    The website version using React.`,
@@ -127,7 +126,7 @@ export const projects = [
     {
         title: 'Where is Waldo',
         image: Waldo,
-        description: "This application is based on the game Where's Waldo, which consists of finding certain characters in an image full of various elements. The application has a timer to measure the time it takes users to find the three characters. The accumulated time is saved in the database along with a username. On the other hand, the game has responsive qualities, since the image can be adapted to different screen sizes without losing its radius of appearance.",
+        description: "This application is inspired by the popular game 'Where's Waldo,' where the challenge lies in spotting specific characters within a crowded image filled with various elements. The app includes a timer to gauge how quickly users can locate three characters. User performance data, including the time taken, is stored in the database along with their usernames. Additionally, the game is designed to be responsive, ensuring that the image adapts seamlessly to various screen sizes.",
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'CSS'],
         github: 'https://github.com/Houser97/Where-is-walso-V2',
         preview: 'https://whereisgame.onrender.com/',
@@ -135,13 +134,13 @@ export const projects = [
     {
         title: 'Shopping Cart',
         image: Shopping,
-        description: `The project simulates a Shopping Cart page with the following features:
+        description: `The project emulates a Shopping Cart page with several noteworthy features:
 
-        \u2022 Available items can be commented and voted based on user experience.\t
-        \u2022 Comments can be liked or disliked.\t
-        \u2022 Items can be added to the user's cart, where the total amount in the cart and the price to pay is shown.\t
+        \u2022 Users can comment on available items and provide ratings based on their experiences.\t
+        \u2022 Comments are interactive, allowing users to like or dislike them.\t
+        \u2022 Items can be added to the user's cart, displaying the total amount in the cart and the price to be paid.\t
         
-        Users can register thanks to the implementation of Node.js and MongoDB as server and database. On the other hand, Redux Toolkit was implemented for global status management. Also, Tailwind is used for the styling of the application.`,
+        User registration is facilitated through the use of Node.js and MongoDB as the server and database technologies. Additionally, Redux Toolkit has been implemented for efficient global state management, while Tailwind CSS has been employed for styling the application`,
         technologies: ['Tailwind CSS','Redux','MongoDB', 'Express.js', 'React', 'Node.js', 'Passport'],
         github: 'https://github.com/Houser97/Shopping-Cart',
         preview: 'https://shopping-cart-a2.onrender.com/',
@@ -149,7 +148,9 @@ export const projects = [
     {
         title: 'Disney Replica',
         image: Disney,
-        description: "The application allows users to create an account through Passport, where the SALTING feature is added to add protection to the passwords entered. Users can choose a profile picture from the images provided. Also, they have the possibility to update this profile picture at any time. On the other hand, the WATCHLIST section shows the movies or series that users have added to their list, which indicate with a checkmark SVG that the card has been added. Users can also remove movies from this section by clicking on the SVG. In total about 21 images were used.",
+        description: `The application enables users to create an account using Passport, which incorporates a salting feature to enhance password security. Users can personalize their profiles by selecting a profile picture from a range of provided images, with the flexibility to update it at their convenience. 
+        
+        Moreover, the application features a WATCHLIST section that displays movies or series added by users. These selections are indicated by a checkmark SVG, signifying that the content has been successfully added. Users can easily remove items from their watchlist by clicking on the SVG. In total, approximately 21 images were utilized to enhance the user experience.`,
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'Passport', 'CSS', 'TypeScript'],
         github: 'https://github.com/Houser97/Disney-V2',
         preview: 'https://disneyplus-54yk.onrender.com/',
@@ -157,7 +158,7 @@ export const projects = [
     {
         title: 'My Blog-API',
         image: BlogAPI,
-        description: 'This project takes advantage of the functionalities of the REST API concept to communicate the client (React) with the server (Node JS). In addition, it makes use of a JWT token to authenticate the blog owner and grant editing, deletion and post creation capabilities.',
+        description: 'This project leverages the capabilities of the REST API concept to establish communication between the client (React) and the server (Node.js). Additionally, it implements a JWT token system to authenticate the blog owner, providing them with the authority to edit, delete, and create posts.',
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'JWT', 'CSS'],
         github: 'https://github.com/Houser97/Blog-API',
         preview: 'https://my-blog-api.onrender.com',
