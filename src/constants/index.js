cbc553d0e3cff085902337c9e1ea3dbfc57d31d7
Arturo Rivera <arturo.riverar97@gmail.com>
2023-10-01 12:22:03 -0600

    Improve project descriptions

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,22 +112,21 @@ export const icons = [
 
 export const projects = [
     {
-        title: 'In Touch Io - Messaging App',
+        title: 'In Touch IO - Messaging App',
         image: InTouchIo,
-        description: `This project showcases weather-related information sourced from the OpenWeatherMap API, which is based on the user's specified city. The application provides essential weather data, including: Temperature (available in both Celsius and Fahrenheit), Humidity levels, Atmospheric pressure, Wind speed, Sunrise and Sunset time, Feels Like Temperature, Min & Max Temperature, Current UV index. For this project, two versions were developed:
+        description: `The app uses MongoDB, Express, Node, React, and Socket IO to deliver a complete real-time messaging application. Additionally, Cloudinary is employed to store user images which are used for profile pictures or within messages.
 
-        \u2022    The mobile version using React Native, created with expo
-        \u2022    The website version using React.`,
-        technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'CSS', 'TypeScript', 'Socket IO'],
+        The app is fully responsive, ensuring a seamless user experience across all devices.  Users can either create an account or opt for the demo user provided in the login component to begin interacting with the application's features. Moreover, the authentication process leverages JSON Web Tokens (JWT) to secure user data and protect the application's routes.`,
+        technologies: ['MongoDB', 'Express.js', 'React', 'CSS', 'TypeScript', 'Socket IO', 'JWT'],
         github: 'https://github.com/Houser97/In-Touch-IO',
         preview: 'https://in-touch-io.onrender.com',
     },
     {
         title: `Weather App React And React Native`,
         image: Weather,
-        description: `This project showcases weather-related information sourced from the OpenWeatherMap API, which is based on the user's specified city. The application provides essential weather data, including: Temperature (available in both Celsius and Fahrenheit), Humidity levels, Atmospheric pressure, Wind speed, Sunrise and Sunset time, Feels Like Temperature, Min & Max Temperature, Current UV index. For this project, two versions were developed:
+        description: `This project showcases weather-related information sourced from the OpenWeatherMap API, which is based on the user's specified city. The application provides essential weather data, including: Temperature (available in both Celsius and Fahrenheit), Humidity levels, Atmospheric pressure, Wind speed, Sunrise and Sunset time, Feels Like Temperature, Min & Max Temperature, and Current UV index. For this project, two versions were developed:
 
-        \u2022    The mobile version using React Native, created with expo
+        \u2022    The mobile version is built with React Native and created using Expo. The link to it can be found in the README file of the repository.
         \u2022    The website version using React.`,
         technologies: ['CSS','Redux','React', 'TypeScript'],
         github: 'https://github.com/Houser97/Weather-App',
@@ -136,7 +135,9 @@ export const projects = [
     {
         title: 'Where is Waldo',
         image: Waldo,
-        description: "This application is inspired by the popular game 'Where's Waldo,' where the challenge lies in spotting specific characters within a crowded image filled with various elements. The app includes a timer to gauge how quickly users can locate three characters. User performance data, including the time taken, is stored in the database along with their usernames. Additionally, the game is designed to be responsive, ensuring that the image adapts seamlessly to various screen sizes.",
+        description: `This application is inspired by the popular game 'Where's Waldo,' where the challenge lies in spotting specific characters within a crowded image filled with various elements. 
+        
+        The app includes a timer to gauge how quickly users can locate three characters. The time taken to find all characters is stored in the database along with their usernames. Additionally, the game is designed to be responsive, ensuring that the image adapts seamlessly to various screen sizes.`,
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'CSS'],
         github: 'https://github.com/Houser97/Where-is-walso-V2',
         preview: 'https://whereisgame.onrender.com/',
@@ -144,13 +145,13 @@ export const projects = [
     {
         title: 'Shopping Cart',
         image: Shopping,
-        description: `The project emulates a Shopping Cart page with several noteworthy features:
+        description: `The project emulates a Shopping Cart page with several features:
 
         \u2022 Users can comment on available items and provide ratings based on their experiences.\t
         \u2022 Comments are interactive, allowing users to like or dislike them.\t
         \u2022 Items can be added to the user's cart, displaying the total amount in the cart and the price to be paid.\t
         
-        User registration is facilitated through the use of Node.js and MongoDB as the server and database technologies. Additionally, Redux Toolkit has been implemented for efficient global state management, while Tailwind CSS has been employed for styling the application`,
+        User registration is facilitated through the use of Node.js and MongoDB as the server and database technologies. Additionally, Redux Toolkit has been implemented for efficient global state management, while Tailwind CSS has been employed for styling the application.`,
         technologies: ['Tailwind CSS','Redux','MongoDB', 'Express.js', 'React', 'Node.js', 'Passport'],
         github: 'https://github.com/Houser97/Shopping-Cart',
         preview: 'https://shopping-cart-a2.onrender.com/',
@@ -158,9 +159,9 @@ export const projects = [
     {
         title: 'Disney Replica',
         image: Disney,
-        description: `The application enables users to create an account using Passport, which incorporates a salting feature to enhance password security. Users can personalize their profiles by selecting a profile picture from a range of provided images, with the flexibility to update it at their convenience. 
+        description: `The application enables users to create an account using Passport, which incorporates a salting feature to enhance password security. Users can personalize their profiles by selecting a profile picture from a range of provided images. 
         
-        Moreover, the application features a WATCHLIST section that displays movies or series added by users. These selections are indicated by a checkmark SVG, signifying that the content has been successfully added. Users can easily remove items from their watchlist by clicking on the SVG. In total, approximately 21 images were utilized to enhance the user experience.`,
+        Moreover, the application features a WATCHLIST section users can see the movies or series they've added from the available options. These selections are indicated by a checkmark SVG, signifying that the content has been successfully added. Users can easily remove items from their watchlist by clicking on the SVG. In total, approximately 21 images were utilized.`,
         technologies: ['MongoDB', 'Express.js', 'React', 'Node.js', 'Passport', 'CSS', 'TypeScript'],
         github: 'https://github.com/Houser97/Disney-V2',
         preview: 'https://disneyplus-54yk.onrender.com/',
