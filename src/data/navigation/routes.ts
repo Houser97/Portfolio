d293061992cc883b0648cfbac6b4f652db8d365c
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2026-01-04 22:14:13 -0600

    Set up project routes as variables

---
+++ src/data/navigation/routes.ts
@@ -0,0 +1,29 @@
+interface Route {
+  name: string;
+  route: string;
+}
+
+export const routes: Record<string, Route> = {
+  home: {
+    name: "Home",
+    route: "/Portfolio/",
+  },
+  selectedWork: {
+    name: "Selected Work",
+    route: "/Portfolio/selected-work",
+  },
+  project: {
+    name: "Project",
+    route: "/Portfolio/project/:id",
+  },
+  experiments: {
+    name: "Experiments",
+    route: "/Portfolio/experiments",
+  },
+};
+
+export const menuRoutes = [
+  routes.home,
+  routes.selectedWork,
+  routes.experiments,
+];
