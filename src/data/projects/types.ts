e8ddecfc74bd9bf20c6bc5e88bf387b2fe1b5b96
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2026-01-04 22:12:19 -0600

    Create project types

---
+++ src/data/projects/types.ts
@@ -0,0 +1,37 @@
+export interface Project {
+  id: string;
+
+  title: string;
+  subtitle?: string;
+
+  context: {
+    company: string;
+    location?: string;
+    year: string;
+    role: string;
+    scope: string;
+  };
+
+  stack: string[];
+
+  background: string[];
+  results: string[];
+
+  images: string[];
+
+  review?: {
+    author: string;
+    role?: string;
+    content: string[];
+  };
+
+  tags: string[];
+}
+
+export interface SlideData {
+  slideImg: string;
+  slideTitle: string;
+  slideDescription: string;
+  slideUrl: string;
+  slideTags: string[];
+}
