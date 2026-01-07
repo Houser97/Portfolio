2f642579c32e4e0e04f0ce253d6ba6b33f748e37
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2026-01-06 23:18:44 -0600

    Create cards.ts

---
+++ src/data/cards.ts
@@ -0,0 +1,41 @@
+export interface Card {
+  id: number;
+  title: string;
+  items?: string[];
+}
+
+export const cards: Card[] = [
+  {
+    id: 1,
+    title: "Frontend",
+    items: [
+      "React",
+      "JavaScript",
+      "TypeScript",
+      "CSS",
+      "Tailwind",
+      "GSAP",
+    ],
+  },
+  {
+    id: 2,
+    title: "Backend",
+    items: [
+        "ASP.NET Core", 
+        "Node.js", 
+        "NestJS", 
+        "SQL Server", 
+        "MongoDB"
+    ],
+  },
+  {
+    id: 3,
+    title: "Tech",
+    items: [
+        "Docker",
+        "Git",
+        "Unit Testing (Jest)",
+        "Ignition"
+    ],
+  },
+];
