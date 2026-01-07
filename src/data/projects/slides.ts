b7c706460b63aeacd40e5ee065f4f22548c65945
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2026-01-06 23:18:55 -0600

    Create slides.ts

---
+++ src/data/projects/slides.ts
@@ -0,0 +1,22 @@
+import { routes } from "../navigation/routes";
+import { experiments } from "./experiments";
+import { projects } from "./projects";
+import { SlideData } from "./types";
+
+export const slides: SlideData[] = projects.map((project) => ({
+  id: project.id,
+  slideImg: project.images[0],
+  slideTitle: project.title,
+  slideDescription: project.subtitle || project.background[0],
+  slideTags: project.tags,
+  slideUrl: routes.project.route.replace(":id", project.id),
+}));
+
+export const experimentSlides: SlideData[] = experiments.map((project) => ({
+  id: project.id,
+  slideImg: project.images[0],
+  slideTitle: project.title,
+  slideDescription: project.background[0],
+  slideTags: project.tags,
+  slideUrl: routes.project.route.replace(":id", project.id),
+}));
