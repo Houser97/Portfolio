bc252615ce33a34fb1050089c009593eba49c7d6
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2026-01-06 23:19:00 -0600

    Create spotlight.ts

---
+++ src/data/projects/spotlight.ts
@@ -0,0 +1,10 @@
+export const spotlightProjects = [
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754563/Portfolio/CocaCompressors_xuqjce.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754563/Portfolio/ClariosFurnaces_bbfyyu.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754566/Portfolio/Peco-demoras_rwln8a.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754568/Portfolio/Peco-tags_f2yfzf.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754568/Portfolio/PecoTrend_vxxfqy.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754569/Portfolio/Planner_shim6u.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754566/Portfolio/CocaHistory_wasqlj.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754562/Portfolio/Autoheat_rho9pa.webp",
+];
