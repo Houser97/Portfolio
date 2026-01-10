ba0f62ab9f28b616a8d3b883d8122679787265d1
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2026-01-09 23:48:30 -0600

    Update spotlight.ts
    Update image links and remove excess links.

--- src/data/projects/spotlight.ts
+++ src/data/projects/spotlight.ts
@@ -1,12 +1,7 @@
 export const spotlightProjects = [
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754563/Portfolio/CocaCompressors_xuqjce.webp",
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754563/Portfolio/ClariosFurnaces_bbfyyu.webp",
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754566/Portfolio/Peco-demoras_rwln8a.webp",
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754568/Portfolio/Peco-tags_f2yfzf.webp",
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754568/Portfolio/PecoTrend_vxxfqy.webp",
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754569/Portfolio/Planner_shim6u.webp",
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754566/Portfolio/CocaHistory_wasqlj.webp",
-  "https://res.cloudinary.com/dluwqcce9/image/upload/v1767754562/Portfolio/Autoheat_rho9pa.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1768020389/Portfolio/Coca-Dashboard_gitz6g.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1768020389/Portfolio/Hornos-2-dark_iaba8p.webp",
+  "https://res.cloudinary.com/dluwqcce9/image/upload/v1768020562/Portfolio/Autoheat_oumpsm.jpg",
 ];
 
 export const HeroImage =
